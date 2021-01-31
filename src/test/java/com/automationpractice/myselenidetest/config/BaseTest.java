16bc1b015e985bf9a4506eb1b3b07f28543a9dff
slava <smartnlg.vladislav.boldyrev.@gmail.com>
2021-02-01 00:35:53 +0300

    добавлен метод setUp для настройки тестов под selenoid

--- src/test/java/com/automationpractice/myselenidetest/config/BaseTest.java
+++ src/test/java/com/automationpractice/myselenidetest/config/BaseTest.java
@@ -1,10 +1,22 @@
 package com.automationpractice.myselenidetest.config;
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class BaseTest {
-    {
+
+    @BeforeAll
+    public static void setUp() {
         Configuration.fastSetValue = true;
         Configuration.baseUrl = "http://automationpractice.com/index.php";
+        Configuration.remote = "http://localhost:4444/wd/hub";
+        Configuration.browser = "firefox";
+        Configuration.browserSize = "1920x1080";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
     }
+
 }
